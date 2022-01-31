2bc80735f8cc28502b88567739b5f397b2973250
wb1110 <strengthcoachwb@gmail.com>
2022-01-31 17:20:24 -0600

    signup almost done

--- api/users/usersModel.js
+++ api/users/usersModel.js
@@ -1,4 +1,4 @@
- const db = require('../data/db-config');
+const db = require('../data/db-config');
 
 function find() {
   return db('users')
@@ -13,23 +13,8 @@ function findById(user_id) {
     .where('users.user_id', user_id).first()
 }
 
-async function add({ username, password, role }) { // done for you
-  let created_user_id
-  await db.transaction(async trx => {
-    let role_id_to_use
-    const [role_name] = await trx('roles').where('role', role)
-    if (role_name) {
-      role_id_to_use = role.role_id
-    } else {
-        // WITH POSTGRES WE CAN PASS A "RETURNING ARRAY" AS 2ND ARGUMENT TO knex.insert/update
-        // AND OBTAIN WHATEVER COLUMNS WE NEED FROM THE NEWLY CREATED/UPDATED RECORD
-      const [role_id] = await trx('roles').insert({ role: role })
-      role_id_to_use = role_id
-    }
-    const [user_id] = await trx('users').insert({ username, password, role_id: role_id_to_use })
-    created_user_id = user_id
-  })
-  return findById(created_user_id)
+async function add({ username, password, role }) {
+  return db('users').insert({ username, password, role_id: role }, ['username', 'role_id'])
 }
 
 module.exports = {
--- api/users/usersRouter.js
+++ api/users/usersRouter.js
@@ -5,9 +5,9 @@ const { JWT_SECRET } = require("../secrets"); // use this secret!
 const jwt = require('jsonwebtoken')
 const bcrypt = require("bcryptjs/dist/bcrypt");
 
-router.post("/signup", validateRoleName, (req, res, next) => {
-  const { username, password } = req.body;
-  const { role } = req;
+router.post("/signup", (req, res, next) => {
+  let { username, password, role } = req.body;
+  role === 'owner' ? role = '1' : role = '2'
   const hash = bcrypt.hashSync(password, 8)
   Users.add({ username, password: hash, role })
   .then(newUser => {
